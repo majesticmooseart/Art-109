09f5bfb7f341652fad08a8747abc95bd40df9480
majesticmooseart <99927126+majesticmooseart@users.noreply.github.com>
2025-03-04 12:43:56 -0800

    project 2

---
+++ projects/net-art/home-pg/home.js
@@ -0,0 +1,81 @@
+
+// icons and memes 
+
+document.querySelector("#computer").addEventListener("click", function(){
+    document.querySelector("#meme-1").style.visibility = "visible";
+    alert("Haha what a funny little dude. CLick the disk next...");
+})
+
+document.querySelector("#disk").addEventListener("click", function(){
+    document.querySelector("#meme-2").style.visibility = "visible";
+    alert("Why so serious XD. Now click the page finder ;)")
+})
+
+document.querySelector("#find").addEventListener("click", function(){
+    document.querySelector("#meme-3").style.visibility = "visible";
+    alert("hey from my favorite move! Piracey is a big problem, and can be dangerous so watch out. Click the paper clip now.")
+})
+
+document.querySelector("#clip").addEventListener("click", function(){
+    document.querySelector("#meme-4").style.visibility = "visible";
+    alert("Such doge much wow. These pop-ups are starting to get a bit much :(. Lets click the folder and see whats in there. ");
+})
+document.querySelector("#folder").addEventListener("click", function(){
+    document.querySelector("#meme-5").style.visibility = "visible";
+    alert("AHHH not satisfiying at all!!! Press the game icon now. ");
+})
+document.querySelector("#game").addEventListener("click", function(){
+    document.querySelector("#meme-9").style.visibility = "visible";
+    alert("Oh No SONIC!!!!! You gotta be carful, these pops could be dagerous tred carefully. Lets check our mail, maybe some sent a funny meme.");
+})
+document.querySelector("#mail").addEventListener("click", function(){
+    document.querySelector("#meme-7").style.visibility = "visible";
+    document.querySelector("#virus-1").style.visibility = "visible";
+    alert("MMMMMMM M&M's are my favorite. Uh whats that??? Lets not touch it for now looks dangerous. click the note pad next.");
+})
+document.querySelector("#note").addEventListener("click", function(){
+    document.querySelector("#meme-8").style.visibility = "visible";
+    alert("Hey! This thing is covering our memes >:( . Let's try closing it. ");
+})
+document.querySelector("#virus-1").addEventListener("click", function(){
+    document.querySelector("#virus-2").style.visibility = "visible";
+    alert("AW man that made it WORSE! Let comback to the problem later. Click on the paint icon next.");
+})
+document.querySelector("#paint").addEventListener("click", function(){
+    document.querySelector("#meme-10").style.visibility = "visible";
+    alert("Nice this ones not covered. Gotta love your friendly neighborhood spider-man. Maybe he could help us if he were here. Let's see what got thrown away. Hit the trash icon. ");
+})
+document.querySelector("#trash").addEventListener("click", function(){
+    document.querySelector("#meme-11").style.visibility = "visible";
+    alert("Can't even see the memes now. Maybe we can find something to remove these on the internet lets try");
+})
+document.querySelector("#web").addEventListener("click", function(){
+    document.querySelector("#virus-3").style.visibility = "visible";
+    alert(" Shoot I thought that would work, . Just try closing the one that just came up.");
+})
+document.querySelector("#virus-3").addEventListener("click", function(){
+    document.querySelector("#virus-4").style.visibility = "visible";
+    alert("Ah man now its just spreading!! Quick maybe closing them will work now.");
+})
+document.querySelector("#virus-4").addEventListener("click", function(){
+    document.querySelector("#virus-5").style.visibility = "visible";
+    alert("Welp that does it they're just spreading. looks like they havent managed to block windows defender yet. That might save us, try it, or try closing the new one its up to you.");
+})
+document.querySelector("#virus-5").addEventListener("click", function(){
+    document.querySelector("#virus-6").style.visibility = "visible";
+    alert("Hey this is getting pretty dangerous, we should probably try to save our desktop...");
+})
+document.querySelector("#virus-6").addEventListener("click", function(){
+    document.querySelector("#virus-7").style.visibility = "visible";
+    alert("Okay well now the screens preety much covered. I dont think there's much left to save now - _ -;");
+})
+document.querySelector("#virus-7").addEventListener("click", function(){
+    document.querySelector("#virus-8").style.visibility = "visible";
+    alert("Don't think this such a good idea anymore... :/ ");
+})
+document.querySelector("#virus-8").addEventListener("click", function(){
+    document.querySelector("#virus-9").style.visibility = "visible";
+    alert("Well That's it the desktop is done. Click the windows defender icon to continue.");
+})
+
+
